204a9e3f63f237460ed96ae13596a126d1e885f7
John Thomas Riley <action.tom@hotmail.com>
2019-11-17 05:53:16 +0200

    removed buttun css

--- views/speed-element.js
+++ views/speed-element.js
@@ -65,34 +65,16 @@ export class SpeedElement extends LitElement {
       font-family: Palatino;
     }
 
-    .button {
-      display: inline-block;
-      padding: 15px 25px;
-      font-size: 15px;
-      cursor: pointer;
-      text-align: center;
-      text-decoration: none;
-      outline: none;
-      color: #fff;
-      background-color: #4CAF50;
-      border: none;
-      border-radius: 15px;
-      box-shadow: 0 9px #999;
-      margin:auto;
-    }
-    
-    .button:hover {background-color: #3e8e41}
-    
-    .button:active {
-      background-color: #3e8e41;
-      box-shadow: 0 5px #666;
-      transform: translateY(2px);
-    }
-    
-
     .divOverFlow{
       height: 250px;
       overflow-y: scroll;
+    }
+    .button
+    {
+      width: 150px;
+      text-align: center;
+      margin:0 auto;
+
     }
 
     table {
